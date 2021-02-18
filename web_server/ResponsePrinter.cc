3bf5ce853e95f7924bf3b0e0d8123277a0f94f19
Gleb Petrovichev <gleb.petrovichev@travelportgds.cz>
2021-02-18 20:09:31 +0100

    Added forgotten headers to ResponsePrinter

--- web_server/ResponsePrinter.cc
+++ web_server/ResponsePrinter.cc
@@ -8,8 +8,13 @@ std::string web_server::ResponsePrinter::print(const web_server::Response& respo
   std::stringstream responseStream;
   responseStream << httpTransportEnumNames.protocolNames.at(response.protocol) << " "
                  << static_cast<std::underlying_type_t<Response::StatusCode>>(response.statusCode) << " "
-                 << httpTransportEnumNames.statusReasonMessages.at(response.statusCode) << "\r\n"
-                 << (!response.body.empty() ? "\r\n" + response.body : "");
+                 << httpTransportEnumNames.statusReasonMessages.at(response.statusCode) << "\r\n";
+
+  for (const Response::Header& header : response.headers) {
+    responseStream << header.first << ": " << header.second << "\r\n";
+  }
+
+ responseStream << (!response.body.empty() ? "\r\n" + response.body : "");
 
   return responseStream.str();
 }
